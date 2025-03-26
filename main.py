e6c871de3c52a1a96710c152e2e83fd3324202c7
Giuseppe di Sciacca | PA Imaging <giuseppe.disciacca@pai.local>
2025-03-26 17:38:27 +0100

    Initial structure

---
+++ main.py
@@ -0,0 +1,22 @@
+import numpy as np
+import scipy
+
+def mesh_rectangle(rectangle_params: list):
+    return
+
+def find_nodes():
+    return
+    
+def find_elements():
+    return
+    
+def solve_system()
+    return
+    
+def get_stiffness_matrix():
+    return
+    
+
+if __name__=='__main__':
+    pass
+
